9dac8c98d358d173b877835fd30f62054abfe2d1
Kgasr <karangupta125@gmail.com>
2024-08-07 03:02:22 +0530

    Minor change in package name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='yaml-config-reader',
+    name='yamlconfigreader',
     version='0.1',
     packages=find_packages(),
     install_requires=['PyYAML'],
